6328e0c24711a23f8546188576e2dfb343e39f3f
Abdul Ghaffar <aghaffar570@gmail.com>
2018-06-29 16:10:50 -0400

    single problem data

--- client/store/problem.js
+++ client/store/problem.js
@@ -8,46 +8,36 @@ import history from '../history'
 const GET_SINGLE_PROBLEM = 'GET_SINGLE_PROBLEM'
 
 /**
- * INITIAL STATE
- */
-const initialSingleProblem = {}
-
-/**
  * ACTION CREATORS
  */
 
-const getSingleProblem = problem => ({ type: GET_SINGLE_PROBLEM, problem })
-
+const getSingleProblem = problem => ({type: GET_SINGLE_PROBLEM, problem})
 
 /**
  * THUNK CREATORS
  */
 
-
-export const fetchSingleProblem = (problemId) => {
-    return dispatch => {
-        axios.get(`/api/problems/${problemId}`)
-            .then(res => res.data)
-            .then(singleProblem => {
-                dispatch(getSingleProblem(singleProblem || initialSingleProblem))
-            })
-            .catch(console.error)
-    }
+export const fetchSingleProblem = problemId => {
+  return dispatch => {
+    axios
+      .get(`/api/problems/${problemId}`)
+      .then(res => res.data)
+      .then(singleProblem => {
+        dispatch(getSingleProblem(singleProblem))
+      })
+      .catch(console.error)
+  }
 }
-
-
 
 //Reducer
-export default function (state = initialSingleProblem, action) {
-    switch (action.type) {
-
-        case GET_SINGLE_PROBLEM: {
-            return action.problem
-        }
-
-        default: {
-            return state
-        }
+export default function(state = {}, action) {
+  switch (action.type) {
+    case GET_SINGLE_PROBLEM: {
+      return action.problem
     }
-}
 
+    default: {
+      return state
+    }
+  }
+}
